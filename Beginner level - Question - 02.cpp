406c9835c01af84474657690b3224428a98f3341
Sukhman Singh <112985333+Sukhman2003@users.noreply.github.com>
2025-01-08 23:25:53 +0530

    Update Beginner level - Question - 02.cpp

--- Beginner level - Question - 02.cpp
+++ Beginner level - Question - 02.cpp
@@ -28,3 +28,36 @@ int result=greatest(num1,num2,num3);
 cout<<"The largest number is: "<<result;
 }
 
+//Question 2 -->Corresponding grade using if-else-if statements
+
+#include <iostream>
+using namespace std;
+char grade(int score){
+if(score>=90 && score<=100){
+    return 'A';
+}
+else if (score>=80 && score<=89) {
+        return 'B';
+} 
+else if (score>=70 && score<=79) {
+        return 'C';
+} 
+else if (score>=60 && score<=69) {
+        return 'D';
+} 
+else if(score>=0 && score<=59) {
+        return 'F';
+    }
+else{
+    return 'E';
+}
+}
+int main(){
+int score;
+cout<<"Enter the score: ";
+cin>>score;
+char result=grade(score);
+cout<<"grade: "<<result;
+}
+
+//question 3 -->
