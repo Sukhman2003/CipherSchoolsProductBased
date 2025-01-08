95430c74c1164402a10c59aed8a0a8e07ea8bd9b
Sukhman Singh <112985333+Sukhman2003@users.noreply.github.com>
2025-01-08 23:04:15 +0530

    Beginner level - Question - 02.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,30 @@
-//test code
+
+//Question 1 --> print greatest number
+
+#include <iostream>
+using namespace std;
+int greatest(int num1,int num2,int num3){
+if(num1>num2 && num1>num3 ){
+    return num1;
+}
+else if(num2>num3){
+    return num2;
+}
+else{
+    return num3;
+}
+}
+int main(){
+int num1;
+cout<<"Enter the first number:";
+cin>>num1;
+int num2;
+cout<<"Enter the second number:";
+cin>>num2;
+int num3;
+cout<<"Enter the third number:";
+cin>>num3;
+int result=greatest(num1,num2,num3);
+cout<<"The largest number is: "<<result;
+}
+
