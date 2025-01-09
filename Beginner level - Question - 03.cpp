034bb1978cb67a0e2241e93f23efd0becfea958c
Sukhman Singh <112985333+Sukhman2003@users.noreply.github.com>
2025-01-09 21:37:22 +0530

    Update Beginner level - Question - 03.cpp

--- Beginner level - Question - 03.cpp
+++ Beginner level - Question - 03.cpp
@@ -18,5 +18,25 @@ int result=sum(number);
 cout<<"The sum of the first "<<number<<" natural numbers is: "<<result;
 }
 
-//Question 2 -->
+//Question 2 -->calculates the factorial of N using a loop
+
+#include <iostream>
+using namespace std;
+int factorial(int number){
+    int a=1;
+for(int i=number;i>=1;i--){
+    a=a*i;
+}
+return a;
+}
+int main(){
+int number;
+cout<<"Enter a number: ";
+cin>>number;
+int result=factorial(number);
+cout<<"The factorial of "<<number<<" is: "<<result;
+}
+
+//Question 3 -->Prints the multiplication table of N up to 10 using a loop
+
 
